5f25f3ab27dc4c4f4526aff9a3ded177845089bd
mrmtsu <81202muramatsu@gmail.com>
2021-08-13 06:46:31 +0900

    generics

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-mod vars;
+// mod stack_heap;
+// mod vars;
+// mod ownership;
+// mod generics;
+// mod lifetime;
+// mod structs;
+mod enums;
 
 fn main() {
     // println!("Hello, world!");
-    vars::run();
+    // vars::run();
+    // stack_heap::run();
+    // ownership::run();
+    // generics::run();
+    // lifetime::run();
+    // structs::run();
+    enums::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
